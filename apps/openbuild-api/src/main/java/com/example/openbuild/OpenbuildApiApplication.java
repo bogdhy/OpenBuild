c10f869d489e02eca99ba6bae110548dc5372b1c
Bogdan Niculescu <12337250+bogdhy@users.noreply.github.com>
2023-04-14 15:38:26 +0000

    API initial setup

---
+++ apps/openbuild-api/src/main/java/com/example/openbuild/OpenbuildApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.openbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbuildApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbuildApiApplication.class, args);
+	}
+
+}
---
+++ apps/openbuild-api/src/test/java/com/example/openbuild/OpenbuildApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbuildApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
