78336e840d6da3207db544aa6fd70c081e206670
Bogdan Niculescu <bogdan.niculescu@dip-software.com>
2023-05-07 18:54:31 +0300

    add oauth2 security configuration

--- apps/openbuild-api/src/main/java/com/agreent/openbuild/OpenbuildApiApplication.java
+++ apps/openbuild-api/src/main/java/com/agreent/openbuild/OpenbuildApiApplication.java
@@ -2,8 +2,12 @@ package com.agreent.openbuild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.agreent.openbuild", "org.axonframework", "engineering.everest"})
+@EntityScan(basePackages = {"com.agreent.openbuild", "org.axonframework", "engineering.everest"})
 public class OpenbuildApiApplication {
 
   public static void main(String[] args) {
